23c2aadf18e726a68e2f8680528ede95a8a218e9
1103409364 <1103409364@qq.com>
2020-03-20 22:33:18 +0800

    finished logout'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="home">
     home
+    <el-button type="info" @click="logout">退出</el-button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push('/login');
+    }
+  }
+};
 </script>
 
 <style lang="stylus" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,24 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Login from '../components/Login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', redirect: 'login' },
-  { path: '/login', component: Login }
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: () => import('../components/Home.vue') },
+  { path: '/login', component: () => import('../components/Login.vue') }
 ];
 
 const router = new VueRouter({
   routes
+});
+// 为路由对象添加导航守卫，to 去哪个路径，from 从哪来，next 去哪
+router.beforeEach((to, form, next) => {
+  if (to.path === '/login') return next();
+
+  const token = window.sessionStorage.getItem('token');
+  if (!token) return next('/login');
+  next();
 });
 
 export default router;
